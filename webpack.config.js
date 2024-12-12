7d7c2edf6c48a7bfa992aa1b87ed6902d01caea2
B1aho <wheelsurgut00@gmail.com>
2024-12-12 08:53:21 +0300

    Add webpack.config setup for html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -8,4 +9,9 @@ module.exports = {
     filename: "main.js",
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    }),
+  ],
 };
