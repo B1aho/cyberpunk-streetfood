48fe37fd64de35bc8cd2860b96a5ef8116a0fa68
B1aho <wheelsurgut00@gmail.com>
2024-12-13 10:25:04 +0300

    Change home info

--- src/createHome.js
+++ src/createHome.js
@@ -12,9 +12,16 @@ export function createHome() {
     const descCard = document.createElement("div")
     descCard.classList.add("home-description")
     const header = document.createElement("h1")
-    header.innerText = "First SCI-FI Cafe"
+    header.innerText = "Neon Grill: Feeding the Future Since 2088"
     const description = document.createElement("p")
-    description.innerText = "ADAFEWfew fesfkewofewjafwejf waejfwjfawjfwe"
+    description.innerText = "Neon Grill is more than just street food; it's a culinary rebellion against the mundane.\n\
+     Born in the heart of the neon jungle, we fuse the grit of back-alley diners with the tech of tomorrow. Our chefs? \
+     Part artists, part engineers, crafting dishes that glow as bright as the city’s skyline. \
+     Every bite is a taste of 2088—powered by innovation, spiced with rebellion, and served with a side of adrenaline.\n\
+     Whether you're a wanderer in the grid or a corporate renegade refueling for the next hack, we’ve got the\
+     perfect meal to light up your circuits. Burgers that pulse with flavor, wraps that pack a punch, and \
+     drinks that electrify your senses—this is street food redefined.\nPlug in. Power up. Bite down.\n\
+     Welcome to Neon Grill, where the future tastes better."
     descCard.append(header, description)
     // Create vibe image
     const imgCard = document.createElement("div")
